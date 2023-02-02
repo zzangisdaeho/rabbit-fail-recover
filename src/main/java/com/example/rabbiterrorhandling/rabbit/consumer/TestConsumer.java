01d4078a3c0044e30eb8138364171c3f20d7a98f
issac <issac@ip-192-168-0-143.ap-northeast-2.compute.internal>
2023-02-02 17:35:01 +0900

    retry

--- src/main/java/com/example/rabbiterrorhandling/rabbit/consumer/TestConsumer.java
+++ src/main/java/com/example/rabbiterrorhandling/rabbit/consumer/TestConsumer.java
@@ -21,24 +21,28 @@ public class TestConsumer {
 
     @RabbitListener(queues = "test.dlx.queue")
     public void processMessage(String messageBody, Channel channel, Message message) throws IOException {
-        try {
-            System.out.println("messageBody = " + messageBody);
-            if(!flag){
-                throw new RuntimeException("error");
-            }
-        } catch (Exception e) {
-            // count the number of retries
-            Integer retryCount = message.getMessageProperties().getHeader("retryCount");
-            if(retryCount == null) retryCount = 0;
-            if (retryCount < 2) {
-                // increment the retry count
-                message.getMessageProperties().setHeader("retryCount", retryCount + 1);
-                // requeue the message
-                rabbitTemplate.send(message.getMessageProperties().getReceivedExchange(), message.getMessageProperties().getReceivedRoutingKey(), message);
-            } else {
-                throw new AmqpRejectAndDontRequeueException(e);
-            }
+        System.out.println("messageBody = " + messageBody);
+        if (!flag) {
+            throw new RuntimeException("error");
         }
+//        try {
+//            System.out.println("messageBody = " + messageBody);
+//            if(!flag){
+//                throw new RuntimeException("error");
+//            }
+//        } catch (Exception e) {
+//            // count the number of retries
+//            Integer retryCount = message.getMessageProperties().getHeader("retryCount");
+//            if(retryCount == null) retryCount = 0;
+//            if (retryCount < 2) {
+//                // increment the retry count
+//                message.getMessageProperties().setHeader("retryCount", retryCount + 1);
+//                // requeue the message
+//                rabbitTemplate.send(message.getMessageProperties().getReceivedExchange(), message.getMessageProperties().getReceivedRoutingKey(), message);
+//            } else {
+//                throw new AmqpRejectAndDontRequeueException(e);
+//            }
+//        }
     }
 
 }
